c41f3e4d05c4114e95b7577dde44c14c51c1780a
lipedeoliveira <felipe.vieira.7721@gmail.com>
2024-05-08 01:25:40 -0300

    Upload do projeto de Calculadora em Java

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author felipe
+ */
+import javax.swing.*;
+public class Principal extends JFrame {
+    
+    public static void main(String[]args){
+    
+        MiniCalculadora app = new MiniCalculadora();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    }
+}
